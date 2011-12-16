74eaa8c42b5e755428e21c16e2247a9f9d106fac
Stef Lewandowski <stef@stef.io>
2011-12-16 16:31:59 +0000

    Debug why crack is failing.

--- lib/chargify/parser.rb
+++ lib/chargify/parser.rb
@@ -3,8 +3,10 @@ module Chargify
 
     def parse
       begin
-        JSON.parse(body)
+        puts body
+        Crack::JSON.parse(body)
       rescue => e
+        puts e.message
         raise(Chargify::Error::UnexpectedResponse.new(e.message), body)
       end
     end
